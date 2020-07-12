a073bc48808269a12afdd5b8b0554dda7a9d99a9
Milan Damen <github@darvit.nl>
2020-07-12 17:41:54 +0200

    Initial tokens

---
+++ token.go
@@ -0,0 +1,125 @@
+package quisnix
+
+type TokenType int
+
+const (
+	None TokenType = iota
+
+	// Literal
+	Integer   // 12345
+	Character // 'a'
+	String    // "abc"
+
+	// Identifier
+	Identifier // main
+
+	// Math
+	Add      // +
+	Subtract // -
+	Multiply // *
+	Divide   // /
+	Modulo   // %
+
+	// Assignment
+	Assign         // =
+	AddAssign      // +=
+	SubtractAssign // -=
+	Increment      // ++
+	Decrement      // --
+
+	// Comparison
+	Equal        // ==
+	NotEqual     // !=
+	Less         // <
+	LessOrEqual  // <=
+	Greater      // >
+	GreaterEqual // >=
+	And          // &&
+	Or           // ||
+
+	// Boolean
+	Not // !
+
+	// Delimiting
+	LeftParenthesis  // (
+	RightParenthesis // )
+	LeftBrace        // {
+	RightBrace       // }
+	LeftBracket      // [
+	RightBracket     // ]
+	Comma            // ,
+	Period           // .
+	Semicolon        // ;
+
+	// Keyword
+	Var
+	Type
+	AnyType
+	Func
+	If
+	Else
+	Return
+	For
+	While
+)
+
+type Token interface {
+	Type() TokenType
+
+	Line() int
+	Column() int
+}
+
+type BasicToken struct {
+	tokenType TokenType
+	line      int
+	column    int
+}
+
+func (t BasicToken) Type() TokenType {
+	return t.tokenType
+}
+
+func (t BasicToken) Line() int {
+	return t.line
+}
+
+func (t BasicToken) Column() int {
+	return t.column
+}
+
+type IntegerToken struct {
+	BasicToken
+	integer int
+}
+
+func (t IntegerToken) Integer() int {
+	return t.integer
+}
+
+type CharacterToken struct {
+	BasicToken
+	character rune
+}
+
+func (t CharacterToken) Character() rune {
+	return t.character
+}
+
+type StringToken struct {
+	BasicToken
+	string string
+}
+
+func (t StringToken) String() string {
+	return t.string
+}
+
+type IdentifierToken struct {
+	BasicToken
+	identifier string
+}
+
+func (t IdentifierToken) Identifier() string {
+	return t.identifier
+}
